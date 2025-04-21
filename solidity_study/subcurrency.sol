5e828945b5533428c5b62caea341e5f93c79443b
zhanghaifeng <541099040@qq.com>
2025-04-21 19:00:38 +0800

    '学习内容0421'

---
+++ solidity_study/subcurrency.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.26;
+//下面的合约实现了一个最简单的加密货币
+//币确实可以无中生有地产生，但是只有创建合约的人才能做到（实现一个不同的发行计划也不难）。
+//任何人都可以给其他人转币，不需要注册用户名和密码 —— 所需要的只是以太坊密钥对。
+
+contract Coin {
+    // address 类型的公共变量,关键字“public”让这些变量可以从外部读取
+    // address 类型是一个160位的值，且不允许任何算数操作。这种类型适合存储合约地址或外部人员的密钥对。
+    address public minter;
+    //mapping 引用类型 是一种键值对存储结构,作用是高效地通过键（Key）查找对应的值（Value）
+    //用于存储每个地址的余额
+    mapping (address => uint) public balances;
+
+    // 轻客户端可以通过事件针对变化作出高效的反应
+    event Sent(address from, address to, uint amount);
+
+    // 这是构造函数，只有当合约创建时运行
+    constructor()  {
+        //msg.sender表示当前合约的创建者的地址。在构造函数中，minter 存储创建合约的人的地址。
+        minter = msg.sender;
+    }
+// 如果 mint 被合约创建者外的其他人调用，则什么也不会发生
+// 如果调用mint函数的是合约的创建者，那么就可以用来管理用户的余额，就是铸造代币的数量
+    function mint(address receiver, uint amount) public {
+        if (msg.sender != minter) return;
+        balances[receiver] += amount;//将铸造的金额（amount）加到接收者的余额（balances[receiver]）上
+    }
+
+// send 函数可被任何人用于向他人发送币 (当然，前提是发送者拥有符合数量的币)。
+    function send(address receiver, uint amount) public {
+        if (balances[msg.sender] < amount) return;
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        emit Sent(msg.sender, receiver, amount);
+    }
+}
+
+
+
--- solidity_study/test1.sol
+++ solidity_study/test1.sol
@@ -1,13 +1,28 @@
-pragma solidity ^0.4.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.26;//源代码使用Solidity版本
+//关键字 pragma:编译指令,是告知编译器如何处理源代码的指令的
+
 
 contract SimpleStorage {
     uint storedData;
-
+//可修改storedData状态的函数
     function set(uint x) public {
         storedData = x;
     }
-
+//定义一个不修改合约状态,且返回无符号整数（uint）的函数
     function get() public view returns (uint) {
         return storedData;
     }
+//函数 set 和 get 可以用来变更或取出变量的值。
 }
+
+/* 这个合约，它能允许任何人在合约中存储一个单独的数字，
+并且这个数字可以被世界上任何人访问，
+且没有可行的办法阻止你发布这个数字。 */
+
+/* 当然，任何人都可以再次调用 set ，传入不同的值，覆盖你的数字，
+在以太坊区块链上，每次成功调用 set 函数修改 storedData 的值时，
+这个状态变更会被记录在区块链中，并且是不可篡改的
+ */
+
+

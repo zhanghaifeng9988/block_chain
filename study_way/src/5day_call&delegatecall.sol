faced3694a9c430d58156aa8eae208e14e33e0fa
zhanghaifeng <541099040@qq.com>
2025-05-03 10:59:01 +0800

    '第6天早，学习内容'

--- study_way/src/5day_call&delegatecall.sol
+++ study_way/src/5day_call&delegatecall.sol
@@ -9,10 +9,8 @@ contract Counter {
         counter += 1;
         sender = msg.sender;
     }
-
-
-    fallback() external payable {
-    }
+    // fallback() external payable {
+    // }
 
 }
 
@@ -20,22 +18,34 @@ contract CallTest {
     uint public counter;
     address public sender;
 
+    //调用逻辑：直接调用Counter合约的count()函数
+    //调用结果：Counter合约的： counter+1，sender更新为CallTest合约地址
     function callCount(Counter c) public {
         c.count();
     }
 
-    // 只是调用代码，合约环境还是当前合约。
-    function lowDelegatecallCount(address addr) public {
+    //调用逻辑：让Counter的count()代码在CallTest环境中执行
+    //调用结果：CallTest合约的：  counter+1，sender更新为CallTest合约地址
+    function lowDelegatecallCount(address  addr) public {
+        //用于生成函数调用的ABI编码数据,返回bytes类型的编码结果,bytes 是动态字节数组类型
         bytes memory methodData = abi.encodeWithSignature("count()");
-        addr.delegatecall(methodData);
+        //delegatecall需要原始ABI编码数据作为输入
+        (bool success, ) =addr.delegatecall(methodData);
+        require(success, "Delegatecall failed"); // 如果失败，回滚交易
     }
 
+    //调用逻辑：通过call调用Counter合约的count()函数
+    //调用结果：Counter合约的counter+1，sender更新为CallTest合约地址
     function lowCallCount(address addr) public {
         bytes memory methodData =abi.encodeWithSignature("count()");
-        addr.call(methodData);
+        (bool success, ) =addr.call(methodData);
+        require(success, "Delegatecall failed");
         // addr.call{gas:1000}(methodData);
         // addr.call{gas:1000, value: 1 ether}(methodData);
     }
+
+    //call 和 传入合约类型参数直接调用得结果一样
+    //delegatecall调用得合约可以比做是类库，调用结果影响当前合约得状态
 
 }
 
